e0f0d6024d6eb6ca9fd366cfc1a03d2a48b40615
jholmes16 <jahmaul74@gmail.com>
2013-08-19 14:24:50 -0400

    Revert "insert data"
    This reverts commit 1d0887bfbe6b157838ddf2365b6490d6554a733b.

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$conn = mysql_connect('localhost', 'root', 'root') or die(mysql_error());
-	echo "Connected to MYSQL<br /><hr />";
-	$db	= mysql_select_db('test') or die(mysql_error());
-	echo "Connected to test<br /><hr />";
-	
-?>
